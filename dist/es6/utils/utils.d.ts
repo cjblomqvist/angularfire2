f65fc3b3258de8cf4edb4ef1059a3b46a0e88297
cjblomqvist <carl@cjblomqvist.com>
2016-06-09 09:44:06 +0200

    add dist for publishing

---
+++ dist/es6/utils/utils.d.ts
@@ -0,0 +1,15 @@
+import { AFUnwrappedDataSnapshot } from './firebase_list_observable';
+export declare function isPresent(obj: any): boolean;
+export declare function isString(value: any): boolean;
+export declare function isFirebaseRef(value: any): boolean;
+export declare function isFirebaseDataSnapshot(value: any): boolean;
+export declare function isAFUnwrappedSnapshot(value: any): boolean;
+export declare function isFirebaseQuery(value: any): boolean;
+export declare function isEmptyObject(obj: Object): boolean;
+export interface CheckUrlRef {
+    isUrl: () => any;
+    isRef: () => any;
+    isQuery?: () => any;
+}
+export declare function unwrapMapFn(snapshot: FirebaseDataSnapshot): AFUnwrappedDataSnapshot;
+export declare function checkForUrlOrFirebaseRef(urlOrRef: string | Firebase | FirebaseQuery, cases: CheckUrlRef): any;
